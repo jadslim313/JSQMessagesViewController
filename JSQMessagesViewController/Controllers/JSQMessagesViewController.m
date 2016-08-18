683a9125f4ea4b2aa92d63475a74efde58979bad
Rayan Slim <rayanslim@Rayans-MacBook-Pro.local>
2016-08-18 15:54:21 -0400

    transform

--- JSQMessagesViewController/Controllers/JSQMessagesViewController.m
+++ JSQMessagesViewController/Controllers/JSQMessagesViewController.m
@@ -177,7 +177,7 @@ JSQMessagesKeyboardControllerDelegate>
 
     self.collectionView.dataSource = self;
     self.collectionView.delegate = self;
-
+    self.collectionView.transform = CGAffineTransformMake(1, 0, 0, -1, 0, 0);
     self.inputToolbar.delegate = self;
     self.inputToolbar.contentView.textView.placeHolder = [NSBundle jsq_localizedStringForKey:@"new_message"];
 
